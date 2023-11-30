9ae5a05f617fa23f9fc738c96fbe751584181132
Backchangdae <backchangdae15@gmail.com>
2023-11-30 15:54:52 -0800

    Final 1

---
+++ TcHmiProject5/File/ShowSelectedItem.js
@@ -0,0 +1,15 @@
+// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.760.42/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var TcHmiProject5;
+        (function (TcHmiProject5) {
+            function ShowSelectedItem(par1) {
+            }
+            TcHmiProject5.ShowSelectedItem = ShowSelectedItem;
+        })(TcHmiProject5 = Functions.TcHmiProject5 || (Functions.TcHmiProject5 = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('ShowSelectedItem', 'TcHmi.Functions.TcHmiProject5', TcHmi.Functions.TcHmiProject5.ShowSelectedItem);
